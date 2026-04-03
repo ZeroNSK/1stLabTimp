30dee04c5338e1812d17eb0a9c6b0c6a5b8e4150
ZeroNSK <peskovskij.ivan@yandex.ru>
2026-04-03 22:32:03 +0700

    4th

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Form from './pages/Form';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Страница не найдена</h1>
+      <p>Такого маршрута нет.</p>
+      <Link to="/">Вернуться на главную</Link>
+    </div>
+  );
+};
 
 const App = () => {
   return (
@@ -12,6 +22,7 @@ const App = () => {
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/add" element={<Form />} />
         <Route path="/edit/:id" element={<Form />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
